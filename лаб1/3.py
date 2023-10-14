984de007f5237dd51deefd68c35b1e8ea3fb2ffc
ugleplastic <127604007+ugleplastic@users.noreply.github.com>
2023-10-14 21:46:18 +0300

    Add files via upload

---
+++ лаб1/3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+team_1 = list_players[0:3]
+team_2 = list_players[3:]
+print(team_1)
+print(team_2)
+
+# TODO Разделите участников на две команды
